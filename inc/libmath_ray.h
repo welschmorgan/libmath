48c9ceac8f10e647f3e159d9fb34ea36a56e01d6
mwelsch <welschmorgan@gmail.com>
2014-02-16 00:16:11 +0100

    Synchronizing Project

--- inc/libmath_ray.h
+++ inc/libmath_ray.h
@@ -6,7 +6,7 @@
 /*   By: mwelsch <mwelsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/14 23:05:53 by mwelsch           #+#    #+#             */
-/*   Updated: 2014/02/14 23:22:40 by mwelsch          ###   ########.fr       */
+/*   Updated: 2014/02/15 23:51:28 by mwelsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBMATH_RAY_H
@@ -24,10 +24,12 @@ typedef struct			s_ray_result
 {
 	int			hit;
 	t_ray		ray;
-	t_real		distance;
+	double		distance;
+	void		*data;
 }						t_ray_result;
 
 t_ray					ray_create(t_vec3 origin, t_vec3 dir);
-t_ray_result			ray_result_create(int hit, t_ray ray, t_real dist);
+t_ray_result			ray_result_create(int hit, t_ray ray,
+										  t_real dist, void *data);
 
 #endif /* !LIBMATH_RAY_H */
--- src/ray_result_create.c
+++ src/ray_result_create.c
@@ -6,18 +6,20 @@
 /*   By: mwelsch <mwelsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/14 23:07:57 by mwelsch           #+#    #+#             */
-/*   Updated: 2014/02/14 23:09:12 by mwelsch          ###   ########.fr       */
+/*   Updated: 2014/02/15 23:51:49 by mwelsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libmath_ray.h"
 
-t_ray_result			ray_result_create(int hit, t_ray ray, t_real dist)
+t_ray_result			ray_result_create(int hit, t_ray ray,
+										  t_real dist, void *data)
 {
 	t_ray_result		res;
 
 	res.hit = hit;
 	res.ray = ray;
 	res.distance = dist;
+	res.data = data;
 	return (res);
 }
