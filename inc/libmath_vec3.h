761e242d20fdeb0a7dc5f193c50f654db19887e1
mwelsch <welschmorgan@gmail.com>
2014-02-16 07:17:19 +0100

    Added cross & dot product

--- inc/libmath_vec3.h
+++ inc/libmath_vec3.h
@@ -6,7 +6,7 @@
 /*   By: mwelsch <mwelsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/13 07:00:10 by mwelsch           #+#    #+#             */
-/*   Updated: 2014/02/14 23:22:58 by mwelsch          ###   ########.fr       */
+/*   Updated: 2014/02/16 07:16:18 by mwelsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBMATH_VEC3_H
@@ -26,6 +26,8 @@ t_vec3					vec3_create(t_real x, t_real y, t_real z);
 t_real					vec3_length(t_vec3 v);
 t_real					vec3_norm(t_vec3 *v);
 t_vec3					vec3_mul(t_vec3 a, t_vec3 b);
+t_real					vec3_dot(t_vec3 a, t_vec3 b);
+t_vec3					vec3_cross(t_vec3 a, t_vec3 b);
 t_vec3					vec3_scale(t_vec3 v, t_real factor);
 t_vec3					vec3_div(t_vec3 a, t_vec3 b);
 t_vec3					vec3_mod(t_vec3 a, t_vec3 b);
