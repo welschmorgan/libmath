05ee593fb7c87a60109ab799c0b44c7a3c215d1b
mwelsch <welschmorgan@gmail.com>
2014-02-19 02:33:41 +0100

    Synchronizing Project

--- inc/libmath_ray.h
+++ inc/libmath_ray.h
@@ -6,7 +6,7 @@
 /*   By: mwelsch <mwelsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/14 23:05:53 by mwelsch           #+#    #+#             */
-/*   Updated: 2014/02/15 23:51:28 by mwelsch          ###   ########.fr       */
+/*   Updated: 2014/02/17 23:09:44 by mwelsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBMATH_RAY_H
@@ -20,16 +20,24 @@ typedef struct			s_ray
 	t_vec3				direction;
 }						t_ray;
 
+typedef struct			s_ray_contact
+{
+	void				*data;
+	t_vec3				point;
+}						t_ray_contact;
+
 typedef struct			s_ray_result
 {
-	int			hit;
-	t_ray		ray;
-	double		distance;
-	void		*data;
+	int					hit;
+	t_ray				ray;
+	double				distance;
+	t_ray_contact		contact;
 }						t_ray_result;
 
 t_ray					ray_create(t_vec3 origin, t_vec3 dir);
 t_ray_result			ray_result_create(int hit, t_ray ray,
 										  t_real dist, void *data);
+t_ray					*ray_init(t_ray *r, t_vec3 origin, t_vec3 dir);
+t_ray_result			*ray_result_init(t_ray_result *r);
 
 #endif /* !LIBMATH_RAY_H */
--- src/ray_init.c
+++ src/ray_init.c
@@ -1,26 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ray_result_create.c                                :+:      :+:    :+:   */
+/*   ray_init.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mwelsch <mwelsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/02/14 23:07:57 by mwelsch           #+#    #+#             */
-/*   Updated: 2014/02/18 02:37:55 by mwelsch          ###   ########.fr       */
+/*   Created: 2014/02/17 23:05:09 by mwelsch           #+#    #+#             */
+/*   Updated: 2014/02/18 02:36:50 by mwelsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libmath_ray.h"
+#include <stddef.h>
 
-t_ray_result			ray_result_create(int hit, t_ray ray,
-										  t_real dist, void *data)
+t_ray					*ray_init(t_ray *r, t_vec3 origin, t_vec3 dir)
 {
-	t_ray_result		res;
+	if (r)
+	{
+		r->origin = origin;
+		r->direction = dir;
+	}
+	return (r);
+}
 
-	res.hit = hit;
-	res.ray = ray;
-	res.distance = dist;
-	res.contact.data = data;
-	res.contact.point = vec3_add(ray.origin, vec3_scale(ray.direction, dist));
-	return (res);
+t_ray_result			*ray_result_init(t_ray_result *r)
+{
+	if (r)
+	{
+		r->hit = 0;
+		ray_init(&r->ray,
+				 vec3_create(0.0f, 0.0f, 0.0f),
+				 vec3_create(0.0f, 0.0f, 0.0f));
+		r->distance = 0.0;
+		r->contact.data = NULL;
+		r->contact.point = r->ray.origin;
+	}
+	return (r);
 }
